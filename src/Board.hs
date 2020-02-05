b4bc0df6133698e9fcd9f5e04db08989a3877a5e
maxfield-chen <maxfieldchen@gmail.com>
2020-02-04 20:15:14 -0800

    Updated record to track captures at a per-game state level. Changed getters / setters to use state. Feeling the pain of modifying nested records, next update will be to replace with lens

--- src/Board.hs
+++ src/Board.hs
@@ -4,6 +4,7 @@ import qualified Data.Map                      as M
 import qualified Data.Set                      as S
 import           Control.Monad.State
 import           Data.Maybe
+import           Data.Functor
 
 
 data Pair a = Pair a a deriving (Show, Eq, Ord)
@@ -17,6 +18,7 @@ instance Applicative Pair where
 
 type Position = Pair Int
 
+-- TODO split outcome into error and outcome to facilitate either
 data Outcome = Nop | Kill | IllegalKo | Suicide | OutOfBounds deriving (Show, Eq)
 -- TODO: Refactor Space to deal with empty case. Maybe instead?
 data Space = Black | White | Empty deriving (Show, Eq, Ord)
@@ -25,33 +27,43 @@ type Board = M.Map Position Space
 
 -- TODO: Refactor record to include captures / active player at each board state
 data Game = Game { boardSize :: Int
-                 , record :: [Board]
-                 , whiteCaptures :: Int
-                 , blackCaptures :: Int
-                 , activePlayer :: Space} deriving (Show)
+                 , record :: [GameState]} deriving (Show)
+
+data GameState = GameState { board :: Board
+                           , toPlay :: Space
+                           , whiteCaptures :: Int
+                           , blackCaptures :: Int} deriving (Show)
 
 data Group = Group { liberties :: S.Set Position
                    , members :: S.Set Position
                    , color :: Space} deriving (Show, Eq)
 
-emptyBoard = M.empty
-newGame = Game 19 [emptyBoard] 0 0 Black
+newGameState = GameState M.empty Black 0 0
+newGame = Game 19 [newGameState]
 
-currentBoard :: Game -> Board
-currentBoard g = if null (record g) then emptyBoard else head (record g)
+currentState :: State Game GameState
+currentState = gets (head . record)
+
+currentBoard :: State Game Board
+currentBoard = board <$> currentState
+
+activePlayer :: State Game Space
+activePlayer = toPlay <$> currentState
 
 getPosition :: Position -> State Game Space
-getPosition pos =
-  state (\game -> (M.findWithDefault Empty pos (currentBoard game), game))
+getPosition pos = M.findWithDefault Empty pos <$> currentBoard
 
 -- TODO: Check if occupied and check if within bounds
 -- TODO: Refactor to return a maybe or either for better error handling?
 setPosition :: Position -> State Game ()
-setPosition pos = state
-  (\game ->
-    let newBoard = M.insert pos (activePlayer game) (currentBoard game)
-    in  ((), game { record = newBoard : record game })
-  )
+setPosition pos = do
+  r        <- gets record
+  newBoard <- M.insert pos <$> activePlayer <*> currentBoard
+  cs       <- currentState
+  g        <- get
+  let _ : rs   = r
+      newState = cs { board = newBoard }
+  put (g { record = newState : rs })
 
 neighborDeltas = [(Pair 0 1), (Pair 0 (-1)), (Pair 1 0), (Pair (-1) 0)]
 
@@ -99,12 +111,13 @@ posToGroup pos = do
 
 -- Place a stone, updating the game record if the move is valid.
 -- Returns an Outcome indicating if the move was valid, and if a group was captured
+-- TODO learn monad transformers so that I can use an either within this state monad to better track the flow of potential failures
 placeStone :: Position -> State Game Outcome
 placeStone pos = do
   setPosition pos
   neighbors <- filterM isOccupied (getNeighbors pos)
   adjGroups <- mapM posToGroup neighbors
-  color     <- get >>= (pure . activePlayer)
+  color     <- activePlayer
   outcome   <- resolvePlacement color adjGroups
   resolveIllegalKo outcome
 
@@ -144,10 +157,9 @@ resolveIllegalKo o = do
 
 isIllegalKo :: State Game Bool
 isIllegalKo =
-  get
-    >>= (pure . record)
+  gets record
     >>= (\r -> case r of
-          k : k' : _ -> pure (k == k')
+          k : k' : _ -> pure (board k == board k')
           _          -> pure False
         )
 
@@ -157,19 +169,21 @@ resolveSuicide =
 
 -- Given a group, remove all members of that group and credit the player with captures
 captureGroup :: Group -> State Game ()
-captureGroup deadGroup = state
-  (\game ->
-    let b : bs   = record game
-        newBoard = M.filterWithKey (isMember deadGroup) b
-        isMember tk pos _ = S.member pos (members tk)
-        newGame = if activePlayer game == Black
-          then game
-            { record        = newBoard : bs
-            , blackCaptures = blackCaptures game + (S.size . members) deadGroup
-            }
-          else game
-            { record        = newBoard : bs
-            , whiteCaptures = whiteCaptures game + (S.size . members) deadGroup
-            }
-    in  ((), newGame)
-  )
+captureGroup deadGroup = do
+  cb <- currentBoard
+  cs <- currentState
+  ap <- activePlayer
+  let newBoard = M.filterWithKey (isMember deadGroup) cb
+      isMember tk pos _ = S.member pos (members tk)
+      newState = if ap == Black
+        then cs
+          { board         = newBoard
+          , blackCaptures = blackCaptures cs + (S.size . members) deadGroup
+          }
+        else cs
+          { board         = newBoard
+          , whiteCaptures = whiteCaptures cs + (S.size . members) deadGroup
+          }
+  newGame <-
+    get >>= (\g -> let s : ss = record g in pure g { record = newState : ss })
+  put newGame

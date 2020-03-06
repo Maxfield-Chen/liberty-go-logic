17c2444b07a920fb0358e07b63a489eda17e93aa
maxfield-chen <maxfieldchen@gmail.com>
2020-03-05 20:42:08 -0800

    Migrated JSON type instances from LGS into LGL game logic for centralization.

--- src/Game.hs
+++ src/Game.hs
@@ -9,6 +9,9 @@ import           Control.Monad.Trans.Except
 import qualified Data.Map                   as M
 import qualified Data.Set                   as S
 import GHC.Generics
+import Data.Aeson hiding (Pair)
+import Data.Aeson.Types hiding (Pair)
+
 
 data Pair a = Pair a a deriving (Show, Eq, Ord, Generic)
 
@@ -20,25 +23,40 @@ instance Applicative Pair where
   (Pair f g) <*> (Pair x y) = Pair (f x) (g y)
 
 type Position = Pair Int
+instance ToJSON (Game.Pair Int)
+instance FromJSON (Game.Pair Int)
+instance ToJSONKey (Game.Pair Int)
+instance FromJSONKey (Game.Pair Int)
 
 data Outcome = NoKill | Kill deriving (Eq, Show, Generic)
+instance ToJSON Outcome
 
 data MoveError = IllegalPlayer |  NoBoard |  IllegalKo | Suicide | OutOfBounds | Occupied deriving (Show, Eq, Generic)
+instance ToJSON MoveError
 
 data Space = Black | White | Empty deriving (Show, Eq, Ord, Generic)
+instance ToJSON Space
+instance FromJSON Space
+instance FromJSONKey Space
+instance ToJSONKey Space
 
 type Board = M.Map Position Space
 
 data Game = Game { _boardSize :: Int
                  , _record    :: [GameState]} deriving (Show, Eq, Generic)
+instance ToJSON Game
+instance FromJSON Game
 
 data GameState = GameState { _board    :: Board
                            , _toPlay   :: Space
                            , _captures :: M.Map Space Int} deriving (Show, Eq, Generic)
+instance ToJSON GameState
+instance FromJSON GameState
 
 data Group = Group { _liberties :: S.Set Position
                    , _members   :: S.Set Position
                    , _player    :: Space} deriving (Show, Eq, Generic)
+instance ToJSON Group
 
 makeLenses ''Game
 makeLenses ''GameState

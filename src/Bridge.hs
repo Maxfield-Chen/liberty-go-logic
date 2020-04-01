16d923ea060703493f257a5cd306e6afab571d58
maxfield-chen <maxfieldchen@gmail.com>
2020-04-01 08:11:31 -0700

    Finish adding Game types to the PureScript Bridge.

--- src/Bridge.hs
+++ src/Bridge.hs
@@ -4,7 +4,14 @@ import           Data.Proxy
 import           Game
 import           Language.PureScript.Bridge
 
-myTypes = [let p = (Proxy :: Proxy Space) in order p (mkSumType p)]
+myTypes = [let p = (Proxy :: Proxy (Pair Int)) in order p (mkSumType p),
+           let p = (Proxy :: Proxy GameStatus) in equal p (mkSumType p),
+           let p = (Proxy :: Proxy Outcome) in equal p (mkSumType p),
+           let p = (Proxy :: Proxy MoveError) in equal p (mkSumType p),
+           let p = (Proxy :: Proxy Space) in order p (mkSumType p),
+           let p = (Proxy :: Proxy Game) in equal p (mkSumType p),
+           let p = (Proxy :: Proxy GameState) in equal p (mkSumType p),
+           let p = (Proxy :: Proxy Group) in equal p (mkSumType p)]
 
 generateModules :: IO ()
 generateModules = writePSTypes "./purescript" (buildBridge defaultBridge) myTypes
